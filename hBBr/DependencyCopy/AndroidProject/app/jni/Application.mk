503bd3337bb2688d67052338cf3f0e25b2000dea
SCcDaniel <836321032@qq.com>
2023-10-08 02:23:18 +0800

    Add libSDL3.so

--- hBBr/DependencyCopy/AndroidProject/app/jni/Application.mk
+++ hBBr/DependencyCopy/AndroidProject/app/jni/Application.mk
@@ -5,7 +5,7 @@
 # APP_STL := c++_shared
 
 #修改APP_ABI，根据实际需求编译生成对应的arm架构的so库。
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := arm64-v8a armeabi-v7a x86_64 x86 
 
 # Min runtime API level
 APP_PLATFORM=android-21
